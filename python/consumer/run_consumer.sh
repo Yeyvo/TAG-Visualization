32073bdf38060bdb6b811e9e01f7d4e9dd78a6c5
ZIANE HICHAM <ziane.hicham.10@gmail.com>
2023-01-30 01:54:01 +0100

    fix10

--- python/consumer/run_consumer.sh
+++ python/consumer/run_consumer.sh
@@ -1,6 +1,6 @@
-sleep 60s
-python3 ./consumer.py &
-sleep 30s 
-python3 ./stat1.py &
-python3 ./stat2.py &
-python3 ./top_line.py 
+sleep 30s
+python3 ./consumer.py #&
+#sleep 30s 
+#python3 ./stat1.py &
+#python3 ./stat2.py &
+#python3 ./top_line.py 
